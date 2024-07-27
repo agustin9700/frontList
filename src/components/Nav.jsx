5a86cac43b4eaeaf87f5d7623440405d69f35f1f
agustin9700 <ryuk10xd@gmail.com>
2024-07-27 16:06:08 -0300

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,7 +20,7 @@ function Nav() {
           } 
           to="/members"
         >
-          Miembros
+          Members
         </NavLink>
       </li>
      
--- src/components/TableRank.jsx
+++ src/components/TableRank.jsx
@@ -1,27 +1,29 @@
-import Celda from "./Celdas"
+import Celda from "./Celdas";
 
 function TableRank() {
   return (
     <main className="col-md-10 col-lg-10 p-4 w-100 bg-light rounded shadow">
-      <h2 className="text-center mb-4 text-dark fw-bold text-uppercase ">List Members</h2>
+      <h2 className="text-center mb-4 text-dark fw-bold text-uppercase ">
+        List Members
+      </h2>
       <div className="table-responsive">
         <table className="table table-hover table-striped">
           <thead className="table-dark">
             <tr className="text-center">
-              <th  scope="col">#</th>
-              <th  scope="col">Nombre</th>
-           <th scope="col">REP ACTUAL</th>
-              <th scope="col">Last 15s</th>
-              <th scope="col">REP 1m</th>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">REP</th>
+              <th scope="col">15s</th>
+              <th scope="col">1m</th>
             </tr>
           </thead>
           <tbody>
-            <Celda/>
+            <Celda />
           </tbody>
         </table>
       </div>
     </main>
-  )
+  );
 }
 
-export default TableRank
+export default TableRank;
